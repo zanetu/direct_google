a2dc37aa82745d857477721085dde4fc3c46af23
zanetu <zanetu@gmail.com>
2016-04-12 18:53:59 -0400

    v2.3.0.6; deleted .project

--- direct_google.user.js
+++ direct_google.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Direct Google
 // @namespace    http://userscripts.org/users/92143
-// @version      2.3.0.5
+// @version      2.3.0.6
 // @description  Removes Google redirects and exposes "Cached" links. 
 // @include      /^https?\:\/\/(www|news|maps|docs|cse|encrypted)\.google\./
 // @author       zanetu
